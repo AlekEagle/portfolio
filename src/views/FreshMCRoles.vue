2913088a7f3976bf1f55b7aad9a3a9253b94176c
Alek Evans <github@alekeagle.com>
2026-08-06 23:28:27 -0800

    add privacy policy for Fresh MC Roles bot

---
+++ src/views/FreshMCRoles.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Privacy Policy for Fresh MC Roles.</h1>
+  <h2>Last Updated: August 6, 2026</h2>
+
+  <p class="pp-body">
+    This Privacy Policy describes how your personal information is collected,
+    used, and shared when you use Fresh MC Roles.
+    <br />
+    <br />
+    <strong>What Data We Collect</strong>
+    <br />
+    <br />
+    We collect your subscription status on Patreon to determine if you have ever
+    supported FreshLX on Patreon, if you have ever supported FreshLX on Patreon,
+    you will receive a role in the Discord server if you have your Discord
+    account linked to your Patreon account. Data is refreshed every 30 minutes.
+    <br />
+    <br />
+    <strong>What Data We Store</strong>
+    <br />
+    <br />
+    Only logs are stored. After 30 days, the logs are deleted. The logs may
+    contain your Discord ID, Discord Username, and Patreon Display Name. The
+    logs are stored for debugging purposes only.
+  </p>
+</template>
+
+<style>
+  .pp-body {
+    font-size: 1.4em;
+    padding: 0 2em;
+  }
+</style>
